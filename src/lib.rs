fc423e0a3005f1ecae2e6c20395e3c757c27e78c
Hartley McGuire <skipkayhil@gmail.com>
2025-11-03 22:47:42 -0500

    Add root page

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,48 @@
-use worker::*;
+use worker::{Context, Env, event};
 
 #[event(fetch)]
-async fn fetch(
-    _req: HttpRequest,
-    _env: Env,
-    _ctx: Context,
-) -> Result<HttpResponse> {
-    Ok(http::Response::builder()
-        .status(http::StatusCode::OK)
-        .body(Body::empty())?)
+async fn fetch(req: http::Request<worker::Body>, _env: Env, _ctx: Context) -> HehirResponse {
+    route(req)
 }
+
+type HehirResponse = http::Result<http::Response<http_body_util::Full<bytes::Bytes>>>;
+
+fn route(_req: http::Request<worker::Body>) -> HehirResponse {
+    http::Response::builder()
+        .status(http::StatusCode::OK)
+        .body(HTML.into())
+}
+
+const HTML: &str = r#"
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <style>
+            body {
+                background: #181818;
+                color: #d8d8d8;
+                font-family: system-ui, sans-serif;
+                max-width: 70ch;
+                padding: 2ch;
+                margin: auto;
+                line-height: 1.75;
+            }
+
+            a {
+                color: #9fd5fc;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>hehir</h1>
+
+        <p>
+            /hɛ(ə)ɹ/
+        </p>
+
+        <p>
+            A <a href="">bunny</a> implementation.
+        </p>
+    </body>
+</html>
+"#;
