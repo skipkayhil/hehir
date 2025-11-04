fe4976b36036afbb174d16df5e43656a378b5211
Hartley McGuire <skipkayhil@gmail.com>
2025-11-04 00:12:14 -0500

    Add gh command and google fallback

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,42 @@ async fn fetch(req: http::Request<worker::Body>, _env: Env, _ctx: Context) -> He
     route(req)
 }
 
-type HehirResponse = http::Result<http::Response<http_body_util::Full<bytes::Bytes>>>;
+type HehirResponse = http::Result<
+    http::Response<
+        http_body_util::Either<
+            http_body_util::Full<bytes::Bytes>,
+            http_body_util::Empty<bytes::Bytes>,
+        >,
+    >,
+>;
 
-fn route(_req: http::Request<worker::Body>) -> HehirResponse {
+fn route(req: http::Request<worker::Body>) -> HehirResponse {
+    match req.uri().path() {
+        "/search" => {
+            let query = req.uri().query().unwrap_or("");
+
+            hop(query)
+        }
+        _ => http::Response::builder()
+            .status(http::StatusCode::OK)
+            .body(http_body_util::Either::Left(HTML.into())),
+    }
+}
+
+fn hop(cmd: &str) -> HehirResponse {
     http::Response::builder()
-        .status(http::StatusCode::OK)
-        .body(HTML.into())
+        .status(http::StatusCode::SEE_OTHER)
+        .header(
+            http::header::LOCATION,
+            Command::from(
+                percent_encoding::percent_decode(cmd.as_bytes())
+                    .decode_utf8()
+                    .unwrap()
+                    .as_ref(),
+            )
+            .to_location(),
+        )
+        .body(http_body_util::Either::Right(http_body_util::Empty::new()))
 }
 
 const HTML: &str = r#"
@@ -46,3 +76,59 @@ const HTML: &str = r#"
     </body>
 </html>
 "#;
+
+enum Command {
+    Github(Option<String>),
+    Google(String),
+}
+
+impl Command {
+    fn to_location(&self) -> String {
+        match self {
+            Command::Github(Some(r)) => format!("https://github.com/{r}"),
+            Command::Github(None) => "https://github.com/".to_string(),
+            Command::Google(q) => format!("https://google.com/search?q={q}"),
+        }
+    }
+}
+
+impl From<&str> for Command {
+    fn from(s: &str) -> Self {
+        let end = s.find(" ");
+        let cmd = &s[..end.unwrap_or(s.len())];
+
+        match cmd {
+            "gh" => match end {
+                Some(e) => Self::Github(Some(s[(e + 1)..].to_string())),
+                None => Self::Github(None),
+            },
+            _ => Self::Google(s.to_string()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn command_parses_gh() {
+        let command: Command = "gh".into();
+
+        assert!(matches!(command, Command::Github(s) if s == None));
+    }
+
+    #[test]
+    fn command_parses_gh_with_arg() {
+        let command: Command = "gh skipkayhil/hehir".into();
+
+        assert!(matches!(command, Command::Github(s) if s == Some("skipkayhil/hehir".to_string())));
+    }
+
+    #[test]
+    fn command_falls_back_to_google() {
+        let command: Command = "g skipkayhil/hehir".into();
+
+        assert!(matches!(command, Command::Google(s) if s == "g skipkayhil/hehir"));
+    }
+}
